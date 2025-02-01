ccb91eff80768f669b5892549b4906a351e261c1
yuri yoshida <yuriyoshida9@gmail.com>
2025-02-01 00:02:01 -0300

    first

---
+++ app/gabi/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){
+    return(
+        <main className="flex justify-center items-center h-screen">
+            <h1>TE AMOOo</h1>
+        </main>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="--font-geist-sans flex flex-col justify-center items-center h-screen">
+      <div>
+        <Link href={'/gabi'}>
+          <RainbowButton className="font-bold">Só clique se vc for a Gabi</RainbowButton>
+        </Link>
+      </div>
+    </main>
+  )
+}
