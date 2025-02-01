024d857abed457b14ff0c0a4e835f0f88c28c4ca
yuri yoshida <yuriyoshida9@gmail.com>
2025-02-01 00:40:46 -0300

    add timer

--- app/gabi/page.tsx
+++ app/gabi/page.tsx
@@ -1,13 +1,42 @@
+'use client'
+import { ShimmerButton } from "@/components/ui/shimmer-button"
 import { TypingAnimation } from "@/components/ui/typing-animation"
+import { useEffect, useState } from "react"
 
 export default function page() {
+
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShow(true);
+        }, 45000);
+        return () => clearTimeout(timer);
+    }, []);
+
+
     return (
-        <main className="flex justify-center items-center ">
+        <main className="flex justify-center items-center flex-col ">
             <div className="p-5">
                 <TypingAnimation className="text-3xl">
                     O fim de tudo será de tranquilidade universal enquanto enfrento minha mortalidade iminente testemunhando o ceifador se aproximando. Desejo que você seja minha visão de morte. O sonho do seu toque sendo minha sensação final traz meu significado de dias numerados enquanto fico acordado à noite ponderando sobre o vasto universo, sabendo que tudo não é nada sem você.
                 </TypingAnimation>
             </div>
+
+            {show ? (
+                <>
+                    <ShimmerButton className="shadow-2xl">
+                        <span className="font-bold rounded-md text-2xl whitespace-pre-wrap text-center leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                            Proxima etapa
+                        </span>
+                    </ShimmerButton>
+                </>
+            ) : (
+                null
+            )
+            }
+
+
         </main>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,7 +63,9 @@ export default {
   		},
   		animation: {
   			rainbow: 'rainbow var(--speed, 2s) infinite linear',
-  			meteor: 'meteor 5s linear infinite'
+  			meteor: 'meteor 5s linear infinite',
+  			'shimmer-slide': 'shimmer-slide var(--speed) ease-in-out infinite alternate',
+  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear'
   		},
   		keyframes: {
   			rainbow: {
@@ -85,6 +87,25 @@ export default {
   				'100%': {
   					transform: 'rotate(215deg) translateX(-500px)',
   					opacity: '0'
+  				}
+  			},
+  			'shimmer-slide': {
+  				to: {
+  					transform: 'translate(calc(100cqw - 100%), 0)'
+  				}
+  			},
+  			'spin-around': {
+  				'0%': {
+  					transform: 'translateZ(0) rotate(0)'
+  				},
+  				'15%, 35%': {
+  					transform: 'translateZ(0) rotate(90deg)'
+  				},
+  				'65%, 85%': {
+  					transform: 'translateZ(0) rotate(270deg)'
+  				},
+  				'100%': {
+  					transform: 'translateZ(0) rotate(360deg)'
   				}
   			}
   		}
