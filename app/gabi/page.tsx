ce49e6d9cc70c59396649b5a2e3097f71117ffaa
yuri yoshida <yuriyoshida9@gmail.com>
2025-02-01 00:23:05 -0300

    fixx

--- app/gabi/page.tsx
+++ app/gabi/page.tsx
@@ -2,7 +2,7 @@ import { TypingAnimation } from "@/components/ui/typing-animation"
 
 export default function page() {
     return (
-        <main className="flex justify-center items-center h-screen">
+        <main className="flex justify-center items-center ">
             <div className="p-5">
                 <TypingAnimation className="text-3xl">
                     O fim de tudo será de tranquilidade universal enquanto enfrento minha mortalidade iminente testemunhando o ceifador se aproximando. Desejo que você seja minha visão de morte. O sonho do seu toque sendo minha sensação final traz meu significado de dias numerados enquanto fico acordado à noite ponderando sobre o vasto universo, sabendo que tudo não é nada sem você.
